f4bef68249bdfff744ee93a6eacb8744298d19f6
baejoonsoo <baejoonsoo21@gmail.com>
2021-10-19 14:57:05 +0900

    brick

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let canvas = document.getElementById('myCanvas');
-let ctx = canvas.getContext('2d');
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
 
-let ballRadius = 10; // 공의 반지름
+const ballRadius = 10; // 공의 반지름
 
 let x = canvas.width / 2; // 공의 x좌표
 let y = canvas.height - 30; // 공의 y좌표
@@ -9,12 +9,20 @@ let y = canvas.height - 30; // 공의 y좌표
 let dx = 2; // 공의 x변화량
 let dy = -2; // 공의 y변화량
 
-let paddleHeight = 10;
-let paddleWidth = 75;
+const paddleHeight = 10;
+const paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
 
 let rightPressed = false;
 let leftPressed = false;
+
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
 
 // 화면에 공을 표시한다
 const drawBall = () => {
@@ -37,14 +45,30 @@ const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // 화면 클리어
 
   drawBall(); // 공을 화면에 표시한다
+  drawPaddle();
+  drawBricks();
 
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     // 죄우 벽면에 부딧힐 시 x 진행 방향을 반전 시킨다
     dx = -dx;
   }
-  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
-    // 상하 벽면에 부딧힐 시 y 진행 방향을 반전 시킨다
+  if (y + dy < ballRadius) {
+    // 위쪽 벽면에 부딧힐 시 y 진행 방향을 반전 시킨다
     dy = -dy;
+  } else if (y + dy > canvas.height - ballRadius) {
+    if (x > paddleX - ballRadius && x < paddleX + paddleWidth + ballRadius) {
+      dy = -dy;
+    } else {
+      alert('GAME OVER');
+      dy = -dy;
+      document.location.reload();
+    }
+  }
+
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 3;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 3;
   }
 
   x += dx;
@@ -54,13 +78,10 @@ const draw = () => {
 const keyDownHandler = (e) => {
   if (e.keyCode == 39) {
     rightPressed = true;
-    console.log('right');
   } else if (e.keyCode == 37) {
     leftPressed = true;
-    console.log('left');
   }
 };
-
 const keyUpHandler = (e) => {
   if (e.keyCode == 39) {
     rightPressed = false;
@@ -68,6 +89,41 @@ const keyUpHandler = (e) => {
     leftPressed = false;
   }
 };
+
+const drawBricks = () => {
+  for (var c = 0; c < brickColumnCount; c++) {
+    for (var r = 0; r < brickRowCount; r++) {
+      var brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+      var brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+
+      bricks[c][r].x = brickX;
+      bricks[c][r].y = brickY;
+
+      ctx.beginPath();
+      ctx.rect(brickX, brickY, brickWidth, brickHeight);
+      ctx.fillStyle = '#0095DD';
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+};
+
+const collisionDetection = () => {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      var b = bricks[c][r];
+      // calculations
+    }
+  }
+};
+
+let bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0 };
+  }
+}
 
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
